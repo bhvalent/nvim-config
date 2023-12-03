fc59ea47fabc89d5215e1b474c0e71b22be8dce2
bhvalent <benhudvalentine@gmail.com>
2023-12-03 15:03:05 -0600

    installing and setting up tree plugin

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -13,6 +13,7 @@ vim.opt.rtp:prepend(lazypath)
 
 require("config.globals")
 require("config.options")
+require("config.keymaps")
 
 local opts = {
 	defaults = {
@@ -26,6 +27,7 @@ local opts = {
 			"gzip",
 			"matchit",
 			"matchparen",
+      "netrw",
 			"netrwPlugin",
 			"tarPlugin",
 			"tohtml",
---
+++ lua/config/keymaps.lua
@@ -0,0 +1,17 @@
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+-- Directory Navigation
+keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
+keymap.set("n", "<leader>ef", ":NvimTreeFocus<CR>", opts)
+
+-- Pane Navigation
+keymap.set("n", "<c-h>", "<c-w>h", opts) -- Navigate Left
+keymap.set("n", "<c-j>", "<c-w>j", opts) -- Navigate Down
+keymap.set("n", "<c-k>", "<c-w>k", opts) -- Navigate Up
+keymap.set("n", "<c-l>", "<c-w>l", opts) -- Navigate Right
+
+-- Window Management
+keymap.set("n", "<leader>sv", ":vsplit<CR>", opts) -- Split Vertically
+keymap.set("n", "<leader>sh", ":split<CR>", opts) -- Split Horizontally
+keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>", opts) -- Toggle Minimize
