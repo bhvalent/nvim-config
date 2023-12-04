c8cf9861c3195b874e0c7217a1c3ffa897b5663b
bhvalent <benhudvalentine@gmail.com>
2023-12-03 22:07:35 -0600

    setting up treesitter

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -20,7 +20,7 @@ local opts = {
 		lazy = true,
 	},
 	install = {
-		colorscheme = { "one_monokai" }
+    colorscheme = { "one_monokai" }
 	},
 	rtp = {
 		disabled_plugins = {
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,40 @@
+local config = function()
+  require("nvim-treesitter.configs").setup({
+    indent = {
+      enable = true,
+    },
+    autotag = {
+      enable = true
+    },
+    ensure_installed = {
+      "markdown",
+      "json",
+      "javascript",
+      "typescript",
+      "html",
+      "css",
+      "scss",
+      "lua",
+      "gitignore",
+      "c_sharp",
+      "go",
+      "gomod",
+      "gosum",
+      "gowork",
+      "regex",
+      "sql",
+      "xml"
+    },
+    auto_intall = true,
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = true
+    }
+  })
+end
+
+return {
+  "nvim-treesitter/nvim-treesitter",
+  config = config,
+  lazy = false,
+}
